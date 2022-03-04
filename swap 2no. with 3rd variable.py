97021d044d02c35238757a5905ff98d9a7d36925
Aniket Vijay Gawde <96537904+GawdeAniketVijay@users.noreply.github.com>
2022-03-04 21:47:42 +0530

    Add files via upload

---
+++ swap 2no. with 3rd variable.py
@@ -0,0 +1,20 @@
+#wap to accept 2no.with using 3rd variable.
+a=int(input("enter a"))
+b=int(input("enter b"))
+print("befor swapping")
+print("a:",a)
+print("b:",b)
+temp=a
+a=b
+b=temp
+print("after swapping")
+print("a:",a)
+print("b:",b)
+
+a=int(input('enter a'))
+b=int(input('enter b'))
+temp=a
+a=b
+b=temp
+print('a',a)
+print('b',b)
