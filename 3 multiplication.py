c3a2a8263c4845913889cc10ce954efd7f7162c8
Aniket Vijay Gawde <96537904+GawdeAniketVijay@users.noreply.github.com>
2022-03-04 21:29:13 +0530

    Add files via upload

---
+++ 3 multiplication.py
@@ -0,0 +1,7 @@
+#wap to accept 3 number and print its multiplication.
+n=int(input("enter a number n:"))
+n1=int(input("enter a number n1:"))
+n2=int(input("enter a number n2:"))
+print(n*n1)
+print(n1*n2)
+print(n2*n)
