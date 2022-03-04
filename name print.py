c411475cdb62ca09a259a94d1b2edbab202fd46b
Aniket Vijay Gawde <96537904+GawdeAniketVijay@users.noreply.github.com>
2022-03-04 21:38:23 +0530

    Add files via upload

---
+++ name print.py
@@ -0,0 +1,28 @@
+'''name=input("enter a name")
+print(name)
+
+names = list["alex", "Hannahe", "James"]
+names.find(e,a)
+print(names)'''
+
+'''num1=int(input('enter a first number'))
+num2=int(input('enter a second number'))
+num3=int(input('enter a third number'))
+if num1>num2 and num1>num3:
+    print(num1)
+if num2>num3 and num2>num1:
+    print(num2)
+else:
+    print(num3)'''
+
+#module of square root
+from math import  sqrt
+print(sqrt(2))
+
+#module of factorial
+from math import fact
+print(fact(5))
+
+#module of power of number
+from math import pow
+print(pow(4,6))
