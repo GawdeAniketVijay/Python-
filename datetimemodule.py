ff4ee8f589342d7d21a96a7d94b3eb34bdfab272
Aniket Vijay Gawde <96537904+GawdeAniketVijay@users.noreply.github.com>
2022-03-04 21:43:52 +0530

    Add files via upload

---
+++ datetimemodule.py
@@ -0,0 +1,11 @@
+'''
+datetime module
+module name:datetime
+classname:datetime
+function:now()=>this function gives system date and time'''
+import datetime
+a=datetime.datetime.now()
+print(a)
+print('minute',a.minute)
+print('second',a.second)
+
