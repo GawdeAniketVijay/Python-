cde6a725ddfe87b9ae66bb349c0296dabdda4b01
Aniket Vijay Gawde <96537904+GawdeAniketVijay@users.noreply.github.com>
2022-03-04 21:30:50 +0530

    Add files via upload

---
+++ swap 2 no. without using 3rd variable.py
@@ -0,0 +1,8 @@
+#wap to accept 2 no. and swap them with out using 3rd variable.
+a=int(input("enter a number a"))
+b=int(input("enter a number b"))
+print("a,b:",3,7)
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
