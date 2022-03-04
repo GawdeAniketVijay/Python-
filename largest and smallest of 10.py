e9cb6976e56467d962711abc1cac30ede321ef2d
Aniket Vijay Gawde <96537904+GawdeAniketVijay@users.noreply.github.com>
2022-03-04 21:50:09 +0530

    Add files via upload

---
+++ largest and smallest of 10.py
@@ -0,0 +1,7 @@
+#wap to accept the 10 values from the user and find the largest and the smallest among them.
+number=[1,2,3,4,5,6,7,8,9,10]
+largest=max(number)
+smallest=min(number)
+print("largest is",largest)
+print("smallest is",smallest)
+print(number[4])
