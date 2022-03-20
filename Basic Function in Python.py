356ed216861fd2c71b00c1bc6b4b38293d83a552
Aniket Vijay Gawde <96537904+GawdeAniketVijay@users.noreply.github.com>
2022-03-20 21:43:26 +0530

    Add files via upload

---
+++ Basic Function in Python.py
@@ -0,0 +1,78 @@
+# LIST
+#gr=["Aniket","Vijay",False,26]
+#print(gr[3])
+
+#a=[1,2,3,4,5,6,2,3,5]
+#print(len(a))
+#a.sort()
+#a.reverse()
+#print(a[2])
+#print(a[0:5]) #Slicing
+#print(len(a))
+#print(max(a))
+#a.append(26) #add
+#print(a)
+#a.insert(1,11) #insert
+#print(a)
+#a.remove(3) #remove
+#a.pop() #remove last number
+#print(a)
+
+
+#SWAP
+'''a=1
+b=5
+a,b=b,a
+print(a,b)'''
+
+
+'''#convert list into tuple
+z=[1,2,3,4,5,6]
+print(tuple(z))
+'''
+
+#TUPLES
+'''b=(1,2,3,4,5,6,2,2)
+print(len(b))
+print(max(b))
+print(min(b))
+print(sum(b))
+print(sorted(b))
+#method tuple
+print(b.index(2))
+print(b.count(2))'''
+#operators in tuple/membership in,not in
+'''print(2 not in b)
+print(b[2])
+
+#For loop
+for x in b:
+    print(x)
+
+for x in range(5):
+    print(x)'''
+
+#SETS
+#c={11,12,13,14,15}
+#c.discard(12)
+#c.remove(11)
+#c.pop()
+#c.clear()
+#c.add(16)
+#c.update([17,18]) #we have to give it in [] then only its going to take.
+#print(c)
+
+
+#Dictionary
+d={1:"Aniket",2:"Vijay",3:"Gawde"}
+print(len(d))
+print(d.keys())
+print(d.values())
+print(d.items()) #show both key and value
+print(d.get(1)) #take key from dict
+print(d.clear()) #clear all record
+
+
+
+
+
